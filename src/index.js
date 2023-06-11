1c0be519cb67caae7c576c27547deeb1aa79232a
nicsanmen <nicsanmen@alum.us.es>
2023-06-11 13:37:31 +0200

    cambios db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from './app.js'
-import {PORT} from './config.js'
+import { PORT } from './config.js'
 
 app.listen(PORT)
-console.log('Server on port', 3000)
+console.log('Server on port', PORT)
